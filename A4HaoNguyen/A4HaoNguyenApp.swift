d084d8e08cf7121f821b119276b79eb57d485282
Hao Nguyen <nguyenquochao2000@gmail.com>
2025-08-03 02:47:06 -0400

    Initial Commit

---
+++ A4HaoNguyen/A4HaoNguyenApp.swift
@@ -0,0 +1,17 @@
+//
+//  A4HaoNguyenApp.swift
+//  A4HaoNguyen
+//
+//  Created by user275773 on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct A4HaoNguyenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A4HaoNguyen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A4HaoNguyen
+//
+//  Created by user275773 on 8/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
