682b65966ebe0167e21121c2c431b44d6a602ca8
Hao Nguyen <nguyenquochao2000@gmail.com>
2025-08-04 03:08:32 -0400

    add dark mode

--- A4HaoNguyen/MenuView.swift
+++ A4HaoNguyen/MenuView.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 struct MenuView: View {
-//    let yesterday = calendar.date(byAdding: .day, value: -1, to: Date())
+    @State private var isLight = true
     @State private var yesterday: Date = {
         Calendar.current.date(byAdding: .day, value: -1, to: Date())!
     }()
-
+    
     @State var list: [TodoItem] = ([
         TodoItem(title: "Do iOS assignment",
                  priority: Priority.Important,
@@ -28,17 +28,32 @@ struct MenuView: View {
                  description: "Test"),
         
     ])
+    let items = ["Item 1", "Item 2", "Item 3", "Item 4"]
     
     var body: some View {
+        Toggle("Light Mode", isOn: $isLight)
+        //                        .tint(.regText)
         NavigationStack{
-            VStack{
+//            Toggle("Light Mode", isOn: $isLight)
+            //                        .tint(.regText)
+            List{
+                
                 ForEach(0..<list.count, id:\.self){ localId in
                     NavigationLink(list[localId].title, destination: ItemView(item: list[localId]))
-                        .navigationTitle(Text("Menu"))
+                    
                 }
+                
             }
-            
+            .navigationTitle(Text("Todo List"))
+            VStack{
+                
+            }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        //        .foregroundStyle(.regText)
+        .background(.colorBg)
+        .preferredColorScheme(isLight ? .light : .dark)
+        
     }
 }
 
